652a8e6cf4e95a8f39d986195fce95d24e6fc987
LJAMINE <aminelmgrmj@gmail.com>
2025-07-21 09:54:25 +0100

    adding the middleware of isAuth verifier is the same id and person

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,3 +6,20 @@ exports.requireSignin = jwt({
   algorithms: ["HS256"],
   requestProperty: "auth",
 });
+
+
+
+
+
+
+exports.isAuth=(req,res,next)=>{
+  let user=req.profile && req.auth && (req.profile._id == req.auth._id);
+
+  if(!user){
+    return res.status(403).json({
+      erro:"Access denied"
+    })
+    
+  }
+  next();
+}
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,16 @@
 const express= require("express");
 const { getOneUser } = require("../controllers/userController");
 const {userById} = require("../middlewares/user");
+const {requireSignin,isAuth} = require("../middlewares/auth");
+
+
 const router = express.Router();
 
 
-router.get('/profile/:userId',getOneUser)
+router.get('/profile/:userId',requireSignin,isAuth,getOneUser)
 
 router.param('userId',userById);
+
 
 
 
